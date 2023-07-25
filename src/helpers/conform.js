fd12cc280869b6b353b80b6a366c5a82c5c36bd3
Bjørn Lindholm <bjornlindholmhansen@gmail.com>
2023-07-25 23:05:03 +0200

    Conform @for loops

--- src/helpers/conform.js
+++ src/helpers/conform.js
@@ -31,5 +31,57 @@ export default function (input) {
 
     output = output.replaceAll('@endif', '</if>')
 
+    // For loops
+    // Check for empty @for directives:
+    // Examples:
+    // @for()
+    // @for(  )
+    if (input.match(/\s*@for\s*\(\s*\)\s*/g)) {
+        throw new CompilationError('No expressions provided to @for directive.')
+    }
+
+    const openingForStatements = [
+        ...input.matchAll(
+            /@for\s*\(\s*(?<variable>\w+)\s*(?:,\s*(?<index>\w+)\s*)?(?:in\s*)?(?<record>.+)?\s*\)/g,
+        ),
+    ]
+    const closingForStatements = [...input.matchAll(/@endfor/g)]
+
+    if (openingForStatements.length !== closingForStatements.length) {
+        throw new CompilationError(
+            'There is an uneven number of opening and closing @for directives.',
+        )
+    }
+
+    for (const match of openingForStatements) {
+        const lexeme = match[0]
+        const variable = match.groups.variable
+        const index = match.groups.index
+        const inOperator = match.groups.in
+        const record = match.groups.record
+
+        if (!variable || variable === 'in') {
+            throw new CompilationError('No variable defined in @for directive.')
+        }
+
+        if (!record || record === 'in') {
+            throw new CompilationError('No record defined in @for directive.')
+        }
+
+        if (index) {
+            output = output.replace(
+                lexeme,
+                `<for variable="${variable.trim()}" record="${record.trim()}" index="${index.trim()}">`,
+            )
+        } else {
+            output = output.replace(
+                lexeme,
+                `<for variable="${variable.trim()}" record="${record.trim()}">`,
+            )
+        }
+    }
+
+    output = output.replaceAll('@endfor', '</for>')
+
     return output
 }
--- tests/helpers/conform.test.js
+++ tests/helpers/conform.test.js
@@ -1,7 +1,7 @@
 import conform from '@/helpers/conform'
 import CompilationError from '@/errors/CompilationError'
 
-describe('If directives', () => {
+describe('@if directives', () => {
     test('it can convert @if to <if>', async () => {
         const input = `<div>
     @if(true)
@@ -127,5 +127,180 @@ describe('If directives', () => {
         const result = conform(input)
 
         expect(result).toBe(expected)
+    })
+
+    test('it can convert if statements inside loops', () => {
+        const input = `<div>
+    @for(item in items)
+        @if(name equals "Yo")
+            <span>Content</span>
+        @endif
+    @endfor
+</div>`
+        const condition = encodeURI('name equals "Yo"')
+        const expected = `<div>
+    <for variable="item" record="items">
+        <if condition="${condition}">
+            <span>Content</span>
+        </if>
+    </for>
+</div>`
+
+        const result = conform(input)
+
+        expect(result).toBe(expected)
+    })
+})
+
+describe('@for directives', () => {
+    test('it can convert @if to <if>', () => {
+        const input = `<div>
+    @for(item in items)
+        <span>{{ item }}</span>
+    @endfor
+</div>`
+        const expected = `<div>
+    <for variable="item" record="items">
+        <span>{{ item }}</span>
+    </for>
+</div>`
+
+        const result = conform(input)
+
+        expect(result).toBe(expected)
+    })
+
+    test('it can convert an index', () => {
+        const input = `<div>
+    @for(item, index in items)
+        <span>{{ item }}</span>
+    @endfor
+</div>`
+        const expected = `<div>
+    <for variable="item" record="items" index="index">
+        <span>{{ item }}</span>
+    </for>
+</div>`
+
+        const result = conform(input)
+
+        expect(result).toBe(expected)
+    })
+
+    test('it ignores whitespace', () => {
+        const input = `<div>
+    @for    (  item,index   in   items  )
+        <span>{{ item }}</span>
+    @endfor
+</div>`
+        const expected = `<div>
+    <for variable="item" record="items" index="index">
+        <span>{{ item }}</span>
+    </for>
+</div>`
+
+        const result = conform(input)
+
+        expect(result).toBe(expected)
+    })
+
+    test('it can convert nested loops', () => {
+        const input = `<div>
+    @for(item in items)
+        @for(item2 in items2)
+            <span>{{ item }}</span>
+        @endfor
+    @endfor
+</div>`
+        const expected = `<div>
+    <for variable="item" record="items">
+        <for variable="item2" record="items2">
+            <span>{{ item }}</span>
+        </for>
+    </for>
+</div>`
+
+        const result = conform(input)
+
+        expect(result).toBe(expected)
+    })
+
+    test('it can convert loops inside if statements', () => {
+        const input = `<div>
+    @if(true)
+        @for(item2 in items2)
+            <span>{{ item }}</span>
+        @endfor
+    @endif
+</div>`
+        const expected = `<div>
+    <if condition="true">
+        <for variable="item2" record="items2">
+            <span>{{ item }}</span>
+        </for>
+    </if>
+</div>`
+
+        const result = conform(input)
+
+        expect(result).toBe(expected)
+    })
+
+    test('it fails if loop info is not provided', () => {
+        const input = `<div>
+    @for()
+        <span>{{ item }}</span>
+    @endfor
+</div>`
+
+        const runner = () => conform(input)
+
+        expect(runner).toThrow(
+            new CompilationError('No expressions provided to @for directive.'),
+        )
+    })
+
+    test('it fails if loop info is no variable', () => {
+        const input = `<div>
+    @for(in items)
+        <span>{{ item }}</span>
+    @endfor
+</div>`
+
+        const runner = () => conform(input)
+
+        expect(runner).toThrow(
+            new CompilationError('No variable defined in @for directive.'),
+        )
+    })
+
+    test('it fails if there is no record', () => {
+        const input = `<div>
+    @for(item in)
+        <span>{{ item }}</span>
+    @endfor
+</div>`
+
+        const runner = () => conform(input)
+
+        expect(runner).toThrow(
+            new CompilationError('No record defined in @for directive.'),
+        )
+    })
+
+    test('it fails if there is not the same number of opening and closing statements', () => {
+        const input = `<div>
+    @for(item in items)
+        @for(item in items)
+        <span>{{ item }}</span>
+    @endfor
+</div>`
+        const runner = () => conform(input)
+
+        expect(runner).toThrow(
+            new CompilationError(
+                'There is an uneven number of opening and closing @for directives.',
+            ),
+        )
     })
 })
